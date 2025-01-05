4788fca47c785039cc8c5318b7084012bea476da
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-05 04:06:22 +0200

    ALLOWED_HOSTS = ['https://djangoprojectstsndev.onrender.com']

--- DjangoProjectStsnDev/settings.py
+++ DjangoProjectStsnDev/settings.py
@@ -25,12 +25,7 @@ SECRET_KEY = 'django-insecure-qw8agq#nes-i4+8iz7tn&x00=8z+ymg+^dbi(7oqqv(dve92h-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+ALLOWED_HOSTS = ['https://djangoprojectstsndev.onrender.com']
 
 # Application definition
 
