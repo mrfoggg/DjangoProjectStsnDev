075c564265ca4f7f87a3b693c1dc826976b545a0
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-06 20:39:32 +0200

    STATIC_ROOT = '/home/h68663c/public_html/mydjangoproject/static'

--- DjangoProjectStsnDev/settings.py
+++ DjangoProjectStsnDev/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = BASE_DIR / 'static/'
+STATIC_URL = '/mydjangoproject/static/'
 # STATIC_ROOT = BASE_DIR / 'static_files'
-STATIC_ROOT = BASE_DIR / 'static_files'
+STATIC_ROOT = '/home/h68663c/public_html/mydjangoproject/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "static_media"
