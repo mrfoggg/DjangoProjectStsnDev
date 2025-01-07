9d7b0d089c06f67a165ca5fd4593ce14024e32d1
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-07 10:06:32 +0200

    OrderSerializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,13 +1,7 @@
-# serializers.py
 from rest_framework import serializers
 from .models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = [
-            'order_id', 'date', 'domain', 'test_domain',
-            'total_amount', 'currency',
-            'customer_name', 'customer_email',
-            'developer_name', 'developer_email'
-        ]
+        fields = '__all__'
