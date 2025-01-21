ffb774a957dc4ec608a5912daeb736b290f954a7
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-21 22:49:55 +0200

    Developer

--- orders/models.py
+++ orders/models.py
@@ -1,24 +1,28 @@
+from django.contrib.postgres.fields import HStoreField
 from django.db import models
 
 class Order(models.Model):
-    order_id = models.IntegerField(unique=True)
+    id = models.IntegerField(unique=True)
     date = models.DateTimeField()
+
+    currency = models.CharField(max_length=10)
+    total_amount = models.DecimalField(max_digits=15, decimal_places=2)
+    commission = models.DecimalField(max_digits=15, decimal_places=2)
+
     domain = models.CharField(max_length=255)
     test_domain = models.CharField(max_length=255, blank=True, null=True)
-    total_amount = models.FloatField()
-    currency = models.CharField(max_length=10)
-    customer_name = models.CharField(max_length=255)
-    customer_email = models.EmailField()
-    developer_name = models.CharField(max_length=255)
-    developer_email = models.EmailField()
 
     def __str__(self):
-        return f"Order {self.order_id} - {self.customer_name}"
+        return f"Order {self.id} - {self.total_amount}"
 
 
-# class Developer(models.Model):
-#     order_id = models.IntegerField(unique=True)
-#     name = models.CharField(max_length=255)
-#     email = models.EmailField()
-#     link = models.URLField(max_length=200, verbose_name="Ссылка на профиль")
+class Developer(models.Model):
+    id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    link = models.URLField(max_length=200, verbose_name="Ссылка на профиль")
+    credits = HStoreField()
+
+    def __str__(self):
+        return f"Order {self.id} - {self.name}"
 
