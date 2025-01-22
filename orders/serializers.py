3ea2dde2c5b28dde537aab8dabdfb52ef68e9c5e
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-22 19:27:04 +0200

    clear6

--- orders/serializers.py
+++ orders/serializers.py
@@ -7,21 +7,23 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
+        print('CREATE')
         developer_id = validated_data.pop('developer_id', None)
         developer_name = validated_data.pop('developer_name', '')
         developer_email = validated_data.pop('developer_email', '')
         developer_link = validated_data.pop('developer_link', '')
         developer_credits = validated_data.pop('developer_credits', {})
 
-        Developer.objects.update_or_create(
-            id=developer_id,
-            defaults={
-                'name': developer_name,
-                'email': developer_email,
-                'link': developer_link,
-                'credits': developer_credits,
-            }
-        )
+        if developer_id:
+            Developer.objects.update_or_create(
+                id=developer_id,
+                defaults={
+                    'name': developer_name,
+                    'email': developer_email,
+                    'link': developer_link,
+                    'credits': developer_credits,
+                }
+            )
 
         # Сохранение Order
         order = Order.objects.create(**validated_data)
