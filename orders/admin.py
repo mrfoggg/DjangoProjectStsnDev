f599b6ba007c7ee20ce7ccac046231fafedd5f25
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-22 11:09:01 +0200

    Игнорирование файлов в migrations, кроме __init__.py

--- orders/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Order
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Order._meta.get_fields()]
