e754541ecf5a094751193f9612105667349d5b30
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-22 20:27:56 +0200

    add other admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.postgres.fields import HStoreField
 from django_json_widget.widgets import JSONEditorWidget
-from .models import Developer, Order
+from .models import Developer, ForumFile, Order, Customer
 
 
 @admin.register(Order)
@@ -9,11 +9,15 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Order._meta.get_fields()]
 
 
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email']
+
+@admin.register(ForumFile)
+class ForumFileAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
 @admin.register(Developer)
 class DeveloperAdmin(admin.ModelAdmin):
     list_display = ['name', 'email']
     readonly_fields = ['credits']
-
-    # formfield_overrides = {
-    #     HStoreField: {'widget': JSONEditorWidget},
-    # }
