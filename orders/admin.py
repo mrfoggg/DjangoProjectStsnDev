555b73d7953b823728bbe3166717c81a0bf18242
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-22 19:11:08 +0200

    2

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.postgres.fields import HStoreField
-# from django_json_widget.widgets import JSONEditorWidget
+from django_json_widget.widgets import JSONEditorWidget
 from .models import Developer, Order
 
 
--- orders/serializers.py
+++ orders/serializers.py
@@ -2,27 +2,16 @@ from rest_framework import serializers
 from .models import Order, Developer
 
 class OrderSerializer(serializers.ModelSerializer):
-    developer_id = serializers.IntegerField(write_only=True)
-    developer_name = serializers.CharField(write_only=True)
-    developer_email = serializers.EmailField(write_only=True)
-    developer_link = serializers.CharField(write_only=True)
-    developer_credits = serializers.HStoreField(write_only=True)
-
     class Meta:
         model = Order
         fields = '__all__'
 
     def create(self, validated_data):
-        print('DDATA', validated_data)
-        developer_credits = validated_data.pop('developer_credits', {})
-        print("CREDITS IN CREATE:", developer_credits)
-        print("CREDITS TYPE IN CREATE:", type(developer_credits))
-
-        # Сохранение Developer
         developer_id = validated_data.pop('developer_id', None)
         developer_name = validated_data.pop('developer_name', '')
         developer_email = validated_data.pop('developer_email', '')
         developer_link = validated_data.pop('developer_link', '')
+        developer_credits = validated_data.pop('developer_credits', {})
 
         if developer_id:
             obj, created = Developer.objects.update_or_create(
