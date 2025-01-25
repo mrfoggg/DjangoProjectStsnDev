048c798996f19dc534f5c5a76a2e5685ac1c932c
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-25 19:19:53 +0200

    main carcass2

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Главная страница
+    path('', views.index, name='home'),  # Главная страница
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(request):
-    print('HOME log')
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request):
+    return render(request, 'index.html')
