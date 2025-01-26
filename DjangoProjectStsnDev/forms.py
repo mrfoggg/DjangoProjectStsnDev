950ee1f840350452082e873a4fc8b682c7f69aae
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 20:41:13 +0200

    18

--- DjangoProjectStsnDev/forms.py
+++ DjangoProjectStsnDev/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class CustomLoginForm(forms.Form):
-    username = forms.CharField(max_length=150, label='Username', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите логин'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль'}))
+    # username = forms.CharField(max_length=150, label='Username', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите логин'}))
+    # password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль'}))
+    pass
