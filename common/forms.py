e01de375e1a5f5a18facfa346b303e12ff54b9c5
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 22:28:40 +0200

    46

--- common/forms.py
+++ common/forms.py
@@ -4,7 +4,3 @@ from django.contrib.auth.forms import AuthenticationForm
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=150, label='Username', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите логин'}))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль'}))
-
-    def __init__(self, *args, **kwargs):
-        self.request = kwargs.get('request')  # Сохраняем объект request
-        super().__init__(*args, **kwargs)
--- common/views.py
+++ common/views.py
@@ -4,8 +4,3 @@ from .forms import LoginForm
 class CustomLoginView(LoginView):
     template_name = 'registration/custom_login.html'
     form_class = LoginForm
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['request'] = self.request  # Передаем объект request в форму
-        return kwargs
