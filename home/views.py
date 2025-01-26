92e72a929e77a1df09ee92479d0abfbbab9dc613
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 19:19:29 +0200

    if form.errors view

--- home/views.py
+++ home/views.py
@@ -1,18 +1,19 @@
-# views.py
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import LoginForm  # Импорт вашей формы
 
 def index(request):
-    if request.method == 'POST' and not request.user.is_authenticated:
+    if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')  # Перенаправление после успешного входа
+            # Если форма валидна, можно выполнять логику логина (например, аутентификацию)
+            # Здесь вы можете добавить логику для авторизации пользователя
+            return HttpResponseRedirect(reverse('home:index'))  # Перенаправление на главную страницу
+        else:
+            # Если форма невалидна, она передается в шаблон с ошибками
+            return render(request, 'index.html', {'form': form})
     else:
-        form = LoginForm()  # Пустая форма для отображения
-    return render(request, 'index.html', {'form': form})
+        # Если метод GET, просто показываем пустую форму
+        form = LoginForm()
+        return render(request, 'index.html', {'form': form})
