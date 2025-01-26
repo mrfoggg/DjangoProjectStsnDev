64e587ff527313ad553344a2ce1b56ed9396d960
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 19:08:34 +0200

    login on home 1

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'home' # app_name используется для пространств имен, чтобы Django мог различать маршруты с одинаковыми именами в разных приложениях.
-
 urlpatterns = [
-    path('', views.index, name='index'),  # Главная страница
+    path('', views.index, name='home'),  # Главная страница
 ]
