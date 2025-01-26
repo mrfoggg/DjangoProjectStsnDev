e2c38bfbef8bebf6c12b3750c3b81500dcd70565
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 16:56:33 +0200

    added home logo link

--- home/apps.py
+++ home/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    name = 'home' # указывает на путь к вашему приложению (путь к приложению в контексте Python-модулей).
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'home' # app_name используется для пространств имен, чтобы Django мог различать маршруты с одинаковыми именами в разных приложениях.
+
 urlpatterns = [
     path('', views.index, name='home'),  # Главная страница
 ]
