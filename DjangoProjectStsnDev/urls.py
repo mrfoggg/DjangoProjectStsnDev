382477e1692379b695e8471ef4e6e50aed788e30
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 18:09:30 +0200

    test5

--- DjangoProjectStsnDev/urls.py
+++ DjangoProjectStsnDev/urls.py
@@ -4,17 +4,17 @@ from django.urls import path, include
 from django.views.i18n import set_language
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('orders/', include('orders.urls')),
     path('', include('home.urls')),
 ]
 
-urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('set_language/', set_language, name='set_language'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    prefix_default_language=False,
-)
+# urlpatterns += i18n_patterns(
+#     path('admin/', admin.site.urls),
+#     path('set_language/', set_language, name='set_language'),
+#     prefix_default_language=False,
+# )
 
-# urlpatterns += [
-#     path('accounts/', include('django.contrib.auth.urls')),
-# ]
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
