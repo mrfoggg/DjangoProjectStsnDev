84603926ce648da5773c5d941488bd9bdede0aba
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 19:01:14 +0200

    login on home

---
+++ home/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150, label='Username', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите логин'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль'}))
--- home/views.py
+++ home/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST' and not request.user.is_authenticated:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Перенаправление после успешного входа
+    else:
+        form = LoginForm()  # Пустая форма для отображения
+    return render(request, 'index.html', {'form': form})
