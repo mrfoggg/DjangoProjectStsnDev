2c8187c154742b60f518052245c640234b6a557d
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 21:50:57 +0200

    31

--- common/urls.py
+++ common/urls.py
@@ -3,9 +3,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
-
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/client_logged_out.html'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/client_logged_out.html'),
          name='client_logout'),
-    path('login/', CustomLoginView.as_view(), name='client_login'),
+    path('accounts/login/', CustomLoginView.as_view(), name='client_login'),
     path('accounts/', include('django.contrib.auth.urls')),  # Стандартные маршруты аутентификации
 ]
