09313eb8891bfc09f37b1ccb62178ddcf6cff1f4
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 20:49:52 +0200

    21

--- DjangoProjectStsnDev/views.py
+++ DjangoProjectStsnDev/views.py
@@ -1,7 +1,8 @@
-# login_views.py
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from .forms import LoginForm
 
 class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
     form_class = LoginForm
+    success_url = reverse_lazy('home')  # Замените 'home' на ваш URL-паттерн
