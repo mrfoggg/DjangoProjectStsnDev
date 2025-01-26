bb714bb299e44b8d7a6b31e9e6ac6120b5803af7
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 20:28:02 +0200

    12

--- DjangoProjectStsnDev/views.py
+++ DjangoProjectStsnDev/views.py
@@ -3,5 +3,5 @@ from django.contrib.auth.views import LoginView
 from .forms import LoginForm
 
 class CustomLoginView(LoginView):
-    template_name = 'registration/login.html'
+    template_name = 'registration/custom_login.html'
     form_class = LoginForm
