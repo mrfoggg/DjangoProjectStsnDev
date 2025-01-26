5fa59918806aa156b686e193e605b8ed7cb65782
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 22:16:19 +0200

    42

--- common/forms.py
+++ common/forms.py
@@ -3,11 +3,11 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=150,
-        label='Username',
+        label='Username_',
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите логин'})
     )
     password = forms.CharField(
-        label='Password',
+        label='Password_',
         widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль'})
     )
 
