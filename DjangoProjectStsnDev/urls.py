4e01b9cad9787e7b0709f88c19c8eadd06e98e45
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 18:24:57 +0200

    test8

--- DjangoProjectStsnDev/urls.py
+++ DjangoProjectStsnDev/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.views.i18n import set_language
 
@@ -16,4 +17,5 @@ urlpatterns += i18n_patterns(
 
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/client_logged_out.html'), name='logout'),
 ]
