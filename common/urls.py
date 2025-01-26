25ea545805803895a24c4dc1f53e777b0ea586ea
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-26 22:14:56 +0200

    41

--- common/urls.py
+++ common/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/client_logged_out.html'),
          name='client_logout'),
     # path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='client_login'),
-    path('accounts/login/', CustomLoginView.as_view(template_name='registration/login.html'), name='client_login'),
+    path('accounts/login/', CustomLoginView.as_view(template_name='registration/custom_login.html'), name='client_login'),
     # path('accounts/', include('django.contrib.auth.urls')),  # Стандартные маршруты аутентификации
 ]
