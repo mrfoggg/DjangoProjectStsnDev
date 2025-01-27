65a76c18ab29b41a6b01c27344c459defd51d59d
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-27 16:13:28 +0200

    user ip_address

--- accounts/models.py
+++ accounts/models.py
@@ -25,6 +25,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField(protocol='both', unpack_ipv4=True, blank=True, null=True)
 
     objects = CustomUserManager()
 
