ccc5fea6aea269dd9a2c1e157aa6905b84ed8068
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-29 00:42:54 +0200

    wsgi py 3

--- DjangoProjectStsnDev/wsgi.py
+++ DjangoProjectStsnDev/wsgi.py
@@ -1,13 +1,12 @@
-import os
 import sys
+import os
 
-# Укажите путь к виртуальному окружению
-activate_this = '/var/www/DjangoProjectStsnDev/venv/bin/activate_this'
-exec(open(activate_this).read(), dict(__file__=activate_this))
+# Путь к вашему виртуальному окружению
+sys.path.insert(0, '/var/www/DjangoProjectStsnDev')
 
-sys.path.append('/var/www/DjangoProjectStsnDev')
-
+# Указываем путь к файлу settings.py
 os.environ['DJANGO_SETTINGS_MODULE'] = 'DjangoProjectStsnDev.settings'
 
+# Импортируем Django
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
