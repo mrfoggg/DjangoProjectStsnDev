3a33d8b10eab17b791d492ad589f4f5397b63891
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-28 11:29:44 +0200

    INSTALLED_APPS = [     "unfold",

--- DjangoProjectStsnDev/settings.py
+++ DjangoProjectStsnDev/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'stsn-dev.kyiv.ua']
 # Application definition
 
 INSTALLED_APPS = [
-    # "unfold",
+    "unfold",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
