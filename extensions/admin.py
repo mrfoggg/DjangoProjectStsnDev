33673c8a0eb90c8cd8cb104ba2799320e2744a0f
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-28 13:22:20 +0200

    print context_data

--- extensions/admin.py
+++ extensions/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.template.context import Context
 from extensions.models import Extension
 from unfold.admin import ModelAdmin
 
@@ -6,3 +7,8 @@ from unfold.admin import ModelAdmin
 class ExtensionAdmin(ModelAdmin):  # Заменяем на стандартный ModelAdmin
     list_display = ['name', 'file_id']
     fields = [('name', 'version'), ('file_id', 'secret_key'), ('file', 'trial_period_days')]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print('context_data____', context)  # Выводим весь контекст в консоль
+        return context
