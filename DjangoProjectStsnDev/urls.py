dba67571a2b48e5a54d2868a8438a8faabcb5f35
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-28 11:38:19 +0200

    path("i18n/", include("django.conf.urls.i18n")),

--- DjangoProjectStsnDev/urls.py
+++ DjangoProjectStsnDev/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("i18n/", include("django.conf.urls.i18n")),
     path('orders/', include('orders.urls')),
     path('', include('home.urls')),
 ]
