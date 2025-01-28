54287142e1e5efc75fef7aa23702f3c8fe68e728
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-29 00:40:53 +0200

    wsgi py 2

--- DjangoProjectStsnDev/wsgi.py
+++ DjangoProjectStsnDev/wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Укажите путь к виртуальному окружению
-activate_this = '/var/www/DjangoProjectStsnDev/venv/bin/activate_this.py'
+activate_this = '/var/www/DjangoProjectStsnDev/venv/bin/activate_this'
 exec(open(activate_this).read(), dict(__file__=activate_this))
 
 sys.path.append('/var/www/DjangoProjectStsnDev')
