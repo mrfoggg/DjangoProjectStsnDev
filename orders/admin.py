eceba00ea6306c1fc3dbd2792ff21f7b1b6db19c
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-28 14:52:29 +0200

    order admins unfold

--- orders/admin.py
+++ orders/admin.py
@@ -1,29 +1,35 @@
 from django.contrib import admin
 from .models import ForumCustomer, Developer, ForumFile, Order, OrderFile
-from django.contrib.admin import TabularInline  # Используем стандартный TabularInline
+from unfold.admin import ModelAdmin, TabularInline
+
 
 class OrderFileInline(TabularInline):  # Можно заменить на StackedInline для другого отображения
     model = OrderFile
     extra = 1  # Количество пустых строк для добавления новых записей
     tab = True
 
+
 @admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):  # Заменяем на стандартный ModelAdmin
+class OrderAdmin(ModelAdmin):
+    # list_display = [field.name for field in Order._meta.get_fields()]
     list_display = ['id', 'date', 'customer', 'total_amount', 'status']
     fields = [('id', 'customer'), 'date', ('currency', 'total_amount', 'commission'), 'status']
 
     inlines = [OrderFileInline]
 
+
 @admin.register(ForumCustomer)
-class CustomerAdmin(admin.ModelAdmin):  # Заменяем на стандартный ModelAdmin
+class CustomerAdmin(ModelAdmin):
     list_display = ['id', 'name', 'email']
 
+
 @admin.register(ForumFile)
-class ForumFileAdmin(admin.ModelAdmin):  # Заменяем на стандартный ModelAdmin
+class ForumFileAdmin(ModelAdmin):
     list_display = ['id', 'name', 'developer']
     readonly_fields = ['extension_name']
 
+
 @admin.register(Developer)
-class DeveloperAdmin(admin.ModelAdmin):  # Заменяем на стандартный ModelAdmin
+class DeveloperAdmin(ModelAdmin):
     list_display = ['id', 'name', 'email']
     readonly_fields = ['credits']
