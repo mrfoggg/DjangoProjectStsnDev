8bc75cdfdd48561f7e0ce6a98127ef0909dcfc3d
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-28 12:20:32 +0200

    test unfold with ExtensionAdmin

--- extensions/admin.py
+++ extensions/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.contrib.admin import TabularInline  # Используем стандартный TabularInline
 from extensions.models import Extension
+from unfold.admin import ModelAdmin
 
 @admin.register(Extension)
-class ExtensionAdmin(admin.ModelAdmin):  # Заменяем на стандартный ModelAdmin
+class ExtensionAdmin(ModelAdmin):  # Заменяем на стандартный ModelAdmin
     list_display = ['name', 'file_id']
     fields = [('name', 'version'), ('file_id', 'secret_key'), ('file', 'trial_period_days')]
