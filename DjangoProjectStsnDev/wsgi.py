06d9385c8cd31261ed43c6d563ccdce45ab2c776
thinkpad-fedora <mrfoggg@gmail.com>
2025-01-31 00:57:07 +0200

    default wsgi

--- DjangoProjectStsnDev/wsgi.py
+++ DjangoProjectStsnDev/wsgi.py
@@ -1,16 +1,6 @@
 import os
-import sys
-
-# Укажите путь к вашему проекту
-sys.path.insert(0, '/var/www/DjangoProjectStsnDev')
-
-# Укажите путь к виртуальному окружению
-sys.path.insert(0, '/var/www/DjangoProjectStsnDev/venv/lib/python3.13/site-packages')
-
-# Установите переменную окружения для Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProjectStsnDev.settings')
-
 from django.core.wsgi import get_wsgi_application
 
-# Импортируйте и создайте WSGI приложение
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
 application = get_wsgi_application()
