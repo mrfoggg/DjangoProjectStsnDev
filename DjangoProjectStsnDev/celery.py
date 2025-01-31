6e828cb963f956051cc31b1b3a9314f4a010bab0
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-01 01:09:04 +0200

    celeru ON

--- DjangoProjectStsnDev/celery.py
+++ DjangoProjectStsnDev/celery.py
@@ -1,23 +1,23 @@
-# from __future__ import absolute_import, unicode_literals
-# import os
-# import sqlite3
-# from celery import Celery
-#
-# # Установите режим WAL для SQLite
-# con = sqlite3.connect('/var/www/DjangoProjectStsnDev/celerydb.sqlite')
-# con.execute('PRAGMA journal_mode=WAL;')
-# con.close()
-#
-# # Установите настройки Django
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProjectStsnDev.settings')
-#
-# app = Celery('DjangoProjectStsnDev')
-#
-# # Используйте настройки Django для конфигурации Celery
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-#
-# # Автоматически находите задачи в приложениях
-# app.autodiscover_tasks()
-#
-# # Поведение для повторных попыток подключения к брокеру
-# app.conf.broker_connection_retry_on_startup = True
+from __future__ import absolute_import, unicode_literals
+import os
+import sqlite3
+from celery import Celery
+
+# Установите режим WAL для SQLite
+con = sqlite3.connect('/var/www/DjangoProjectStsnDev/celerydb.sqlite')
+con.execute('PRAGMA journal_mode=WAL;')
+con.close()
+
+# Установите настройки Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProjectStsnDev.settings')
+
+app = Celery('DjangoProjectStsnDev')
+
+# Используйте настройки Django для конфигурации Celery
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Автоматически находите задачи в приложениях
+app.autodiscover_tasks()
+
+# Поведение для повторных попыток подключения к брокеру
+app.conf.broker_connection_retry_on_startup = True
