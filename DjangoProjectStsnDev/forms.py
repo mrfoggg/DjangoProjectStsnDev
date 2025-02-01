ff0d442a694ac9f84e7860715f45c575c5f9abd4
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-01 03:28:18 +0200

    home remove form2

--- DjangoProjectStsnDev/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=150, label='Username', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите логин'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль'}))
--- home/views.py
+++ home/views.py
@@ -9,4 +9,4 @@ def index(request):
     request.session.modified = True  # Принудительное обновление сессии
     print("Session Key:", request.session.session_key)  # Вывести session_key в консоль
     sys.stdout.flush()  # Принудительный вывод в консоль
-    return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html')
