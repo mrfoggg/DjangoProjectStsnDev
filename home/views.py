5b5772f23bfbbd85184bf94e7286c178e63d21c5
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-01 02:43:50 +0200

    Принудительный вывод в консоль

--- home/views.py
+++ home/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from DjangoProjectStsnDev.forms import LoginForm  # Импорт вашей формы
+import sys
 
 def index(request):
     request.session['test_key'] = 'test_value'  # Запись в сессию
     request.session.modified = True  # Принудительное обновление сессии
 
     print("Session Key:", request.session.session_key)  # Вывести session_key в консоль
+    sys.stdout.flush()  # Принудительный вывод в консоль
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
