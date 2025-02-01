ca2eae4834bc44aebbdd53e431094d8f8fa9d612
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-01 18:25:02 +0200

    set_password_view, cabinet fix9

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import CustomLoginView, send_email_verification_view, email_verification_sent_success_view, cabinet_view, \
-    activate_account_view
+    activate_account_view, set_password_view
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(template_name='custom_login.html'), name='client_login'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('register/', send_email_verification_view, name='send_email_verification_for_register'),
     path('activate_accont/<str:email>/<str:token>/', activate_account_view, name='activate_account'),
     path('email_verification_sent_success/', email_verification_sent_success_view, name='email_verification_sent_success'),
+    path('set_password/', set_password_view, name='set_password'),
     path('cabinet/', cabinet_view, name='cabinet'),
 
 ]
