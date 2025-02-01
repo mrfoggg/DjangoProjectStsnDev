4ea5bf4ae49c742724e222857d40ecdd23db0e1f
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-01 03:25:25 +0200

    home remove form

--- home/views.py
+++ home/views.py
@@ -7,19 +7,6 @@ import sys
 def index(request):
     request.session['test_key'] = 'test_value'  # Запись в сессию
     request.session.modified = True  # Принудительное обновление сессии
-
     print("Session Key:", request.session.session_key)  # Вывести session_key в консоль
     sys.stdout.flush()  # Принудительный вывод в консоль
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            # Если форма валидна, можно выполнять логику логина (например, аутентификацию)
-            # Здесь вы можете добавить логику для авторизации пользователя
-            return HttpResponseRedirect(reverse('home'))  # Перенаправление на главную страницу
-        else:
-            # Если форма невалидна, она передается в шаблон с ошибками
-            return render(request, 'index.html', {'form': form})
-    else:
-        # Если метод GET, просто показываем пустую форму
-        form = LoginForm()
-        return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html', {'form': form})
