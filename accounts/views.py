dd03edbdeae5f2ca564c4b43a7fc968e7bd20a7c
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-01 19:53:49 +0200

    debug10

--- accounts/views.py
+++ accounts/views.py
@@ -100,7 +100,6 @@ def set_password_view(request):
     print('DEBUG - request.user.is_authenticated -', request.user.is_authenticated)
     print('DEBUG - request.user.has_usable_password() -', request.user.has_usable_password())
     print('DEBUG - request.user.password -', request.user.password)
-    if request.method == 'POST':
     if request.user.is_authenticated and not request.user.has_usable_password() and request.user.password != '':
         if request.method == 'POST':
             form = SetPasswordFormWithConfirmation(user=request.user, data=request.POST)
