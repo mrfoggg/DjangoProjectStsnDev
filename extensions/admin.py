eb610d8e8087c11a755a1f107ac79bb171ff3a2c
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-02 21:20:04 +0200

    get_form DEBUG get_fields_

--- extensions/admin.py
+++ extensions/admin.py
@@ -25,7 +25,7 @@ class ExtensionAdmin(ModelAdmin):
         Переопределяем get_form для использования нашей кастомной формы.
         """
         form = super().get_form(request, obj, **kwargs)
-        print('DEBUG filelds - ', self.get_fields())
+        print('DEBUG filelds - ', self.get_fields(request))
         return form
 
     def save_model(self, request, obj, form, change):
