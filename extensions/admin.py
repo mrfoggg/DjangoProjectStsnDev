b6f1ec87d19c7b9c44f3c35d6feabcbd3131a73a
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-03 01:00:43 +0200

    proxy50023 with ExtensionProxyForm

--- extensions/admin.py
+++ extensions/admin.py
@@ -1,47 +1,7 @@
 from django.contrib import admin
-from .models import Extension, ExtensionTranslation, ExtensionProxy
 from unfold.admin import ModelAdmin
-from django import forms
-
-
-@admin.register(ExtensionTranslation)
-class ExtensionTranslationAdmin(ModelAdmin):
-    list_display = ('extension', 'language_code', 'name')
-
-
-class ExtensionProxyForm(forms.ModelForm):
-    """Форма для работы с виртуальными полями в ExtensionProxy"""
-
-    name_en = forms.CharField(label="Название (EN)", required=False)
-    description_en = forms.CharField(label="Описание (EN)", widget=forms.Textarea, required=False)
-    name_ru = forms.CharField(label="Название (RU)", required=False)
-    description_ru = forms.CharField(label="Описание (RU)", widget=forms.Textarea, required=False)
-
-    class Meta:
-        model = ExtensionProxy
-        fields = ['name', 'version', 'secret_key', 'trial_period_days']
-
-    def __init__(self, *args, **kwargs):
-        """Заполняем форму данными из модели"""
-        super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
-        if instance:
-            self.fields['name_en'].initial = instance.name_en
-            self.fields['description_en'].initial = instance.description_en
-            self.fields['name_ru'].initial = instance.name_ru
-            self.fields['description_ru'].initial = instance.description_ru
-
-    def save(self, commit=True):
-        """Сохраняем данные виртуальных полей"""
-        instance = super().save(commit=False)
-        instance.name_en = self.cleaned_data['name_en']
-        instance.description_en = self.cleaned_data['description_en']
-        instance.name_ru = self.cleaned_data['name_ru']
-        instance.description_ru = self.cleaned_data['description_ru']
-        if commit:
-            instance.save()
-        return instance
-
+from .models import ExtensionProxy
+from .forms import ExtensionProxyForm
 
 @admin.register(ExtensionProxy)
 class ExtensionProxyAdmin(ModelAdmin):
--- extensions/forms.py
+++ extensions/forms.py
@@ -1,69 +1,51 @@
 from django import forms
-from .models import Extension, ExtensionTranslation
-from django.utils.translation import gettext_lazy as _
-from DjangoProjectStsnDev import settings
+from .models import ExtensionProxy
 
+class ExtensionProxyForm(forms.ModelForm):
+    """Форма для работы с виртуальными полями в ExtensionProxy"""
 
-class ExtensionAdminForm(forms.ModelForm):
-    """
-    Кастомная форма для модели Extension с полями переводов.
-    """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Добавляем поля перевода только в форму
-        if self.instance.pk:
-            translations = ExtensionTranslation.objects.filter(extension=self.instance)
-            existing_langs = {t.language_code for t in translations}
-
-            for lang_code, lang_name in settings.LANGUAGES:
-                # Проверка на наличие перевода, если перевода нет, создаем пустое поле
-                if lang_code not in existing_langs:
-                    ExtensionTranslation.objects.create(extension=self.instance, language_code=lang_code)
-
-                self.fields[f'name_{lang_code}'] = forms.CharField(
-                    initial=translations.filter(language_code=lang_code).first().name if translations.filter(
-                        language_code=lang_code).exists() else '',
-                    label=f'Name ({lang_code})',
-                    max_length=255,
-                    required=False
-                )
-
-                self.fields[f'description_{lang_code}'] = forms.CharField(
-                    initial=translations.filter(language_code=lang_code).first().description if translations.filter(
-                        language_code=lang_code).exists() else '',
-                    label=f'Description ({lang_code})',
-                    widget=forms.Textarea,
-                    required=False
-                )
-
-                self.fields[f'short_description_{lang_code}'] = forms.CharField(
-                    initial=translations.filter(
-                        language_code=lang_code).first().short_description if translations.filter(
-                        language_code=lang_code).exists() else '',
-                    label=f'Short Description ({lang_code})',
-                    max_length=255,
-                    required=False
-                )
-
-                self.fields[f'title_{lang_code}'] = forms.CharField(
-                    initial=translations.filter(language_code=lang_code).first().title if translations.filter(
-                        language_code=lang_code).exists() else '',
-                    label=f'Title ({lang_code})',
-                    max_length=255,
-                    required=False
-                )
-
-                self.fields[f'meta_description_{lang_code}'] = forms.CharField(
-                    initial=translations.filter(
-                        language_code=lang_code).first().meta_description if translations.filter(
-                        language_code=lang_code).exists() else '',
-                    label=f'Meta Description ({lang_code})',
-                    widget=forms.Textarea,
-                    required=False
-                )
+    name_en = forms.CharField(
+        label="Название (EN)",
+        required=False,
+        widget=forms.TextInput()
+    )
+    description_en = forms.CharField(
+        label="Описание (EN)",
+        required=False,
+        widget=forms.Textarea()
+    )
+    name_ru = forms.CharField(
+        label="Название (RU)",
+        required=False,
+        widget=forms.TextInput()
+    )
+    description_ru = forms.CharField(
+        label="Описание (RU)",
+        required=False,
+        widget=forms.Textarea()
+    )
 
     class Meta:
-        model = Extension
-        fields = '__all__'  # Включаем только поля модели, без переводных полей
+        model = ExtensionProxy
+        fields = ['name', 'version', 'secret_key', 'trial_period_days']
+
+    def __init__(self, *args, **kwargs):
+        """Заполняем форму данными из модели"""
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get('instance')
+        if instance:
+            self.fields['name_en'].initial = instance.name_en
+            self.fields['description_en'].initial = instance.description_en
+            self.fields['name_ru'].initial = instance.name_ru
+            self.fields['description_ru'].initial = instance.description_ru
+
+    def save(self, commit=True):
+        """Сохраняем данные виртуальных полей"""
+        instance = super().save(commit=False)
+        instance.name_en = self.cleaned_data['name_en']
+        instance.description_en = self.cleaned_data['description_en']
+        instance.name_ru = self.cleaned_data['name_ru']
+        instance.description_ru = self.cleaned_data['description_ru']
+        if commit:
+            instance.save()
+        return instance
