88b400db47797ce1c4454e3d14235f447fdfff48
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-02 23:52:37 +0200

    Proxy Model / admin var 2

--- extensions/admin.py
+++ extensions/admin.py
@@ -12,8 +12,34 @@ class ExtensionAdmin(admin.ModelAdmin):
     search_fields = ('name', 'version', 'secret_key')
 
 class ExtensionProxyAdmin(ExtensionAdmin):
-    list_display = ('name', 'name_en', 'name_ru', 'description_en', 'description_ru')
-    search_fields = ('name', 'name_en', 'name_ru', 'description_en', 'description_ru')
+    list_display = (
+        'name_all_languages',
+        'description_all_languages',
+        'short_description_all_languages',
+        'title_all_languages',
+        'meta_description_all_languages',
+    )
+    search_fields = ('name', 'description', 'short_description', 'title', 'meta_description')
+
+    def name_all_languages(self, obj):
+        return f"EN: {obj.name_en} | RU: {obj.name_ru}"
+    name_all_languages.short_description = 'Name'
+
+    def description_all_languages(self, obj):
+        return f"EN: {obj.description_en} | RU: {obj.description_ru}"
+    description_all_languages.short_description = 'Description'
+
+    def short_description_all_languages(self, obj):
+        return f"EN: {obj.short_description_en} | RU: {obj.short_description_ru}"
+    short_description_all_languages.short_description = 'Short Description'
+
+    def title_all_languages(self, obj):
+        return f"EN: {obj.title_en} | RU: {obj.title_ru}"
+    title_all_languages.short_description = 'Title'
+
+    def meta_description_all_languages(self, obj):
+        return f"EN: {obj.meta_description_en} | RU: {obj.meta_description_ru}"
+    meta_description_all_languages.short_description = 'Meta Description'
 
 admin.site.register(Extension, ExtensionAdmin)
 admin.site.register(ExtensionProxy, ExtensionProxyAdmin)
