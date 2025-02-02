edc341c0aa7c48fff3e43ec807c4e70b50f3ae32
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-02 20:37:19 +0200

    ExtensionAdminForm ver 2

--- extensions/admin.py
+++ extensions/admin.py
@@ -8,89 +8,37 @@ from unfold.admin import ModelAdmin
 #     list_display = ['name', 'file_id']
 #     fields = [('name', 'version'), ('file_id', 'secret_key'), ('file', 'trial_period_days')]
 
-from django import forms
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
 from .models import Extension, ExtensionTranslation
-
-class ExtensionAdminForm(forms.ModelForm):
-    """
-    Кастомная форма для модели Extension, в которую добавляются
-    поля для языковых переводов.
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if self.instance.pk:
-            translations = ExtensionTranslation.objects.filter(extension=self.instance)
-            for translation in translations:
-                # Динамически добавляем поля в форму для каждого языка
-                lang_code = translation.language_code
-
-                self.fields[f'name_{lang_code}'] = forms.CharField(
-                    initial=translation.name,
-                    label=_('Name') + f" ({lang_code})",
-                    max_length=255,
-                    required=False
-                )
-
-                self.fields[f'description_{lang_code}'] = forms.CharField(
-                    initial=translation.description,
-                    label=_('Description') + f" ({lang_code})",
-                    widget=forms.Textarea,
-                    required=False
-                )
-
-                self.fields[f'short_description_{lang_code}'] = forms.CharField(
-                    initial=translation.short_description,
-                    label=_('Short Description') + f" ({lang_code})",
-                    max_length=255,
-                    required=False
-                )
-
-                self.fields[f'title_{lang_code}'] = forms.CharField(
-                    initial=translation.title,
-                    label=_('Title') + f" ({lang_code})",
-                    max_length=255,
-                    required=False
-                )
-
-                self.fields[f'meta_description_{lang_code}'] = forms.CharField(
-                    initial=translation.meta_description,
-                    label=_('Meta Description') + f" ({lang_code})",
-                    widget=forms.Textarea,
-                    required=False
-                )
-
-    class Meta:
-        model = Extension
-        fields = '__all__'
-
+from .forms import ExtensionAdminForm
 
 class ExtensionAdmin(admin.ModelAdmin):
     """
-    Кастомный ModelAdmin для модели Extension,
-    который добавляет редактирование переводов без инлайнов.
+    Кастомный ModelAdmin для Extension с редактированием языковых полей.
     """
     form = ExtensionAdminForm
     list_display = ('name', 'version', 'secret_key')
 
     def save_model(self, request, obj, form, change):
         """
-        Переопределяем сохранение модели, чтобы обработать сохранение переводов.
+        Сначала сохраняем основной объект, затем обрабатываем переводы.
         """
         super().save_model(request, obj, form, change)
 
-        for field_name, value in form.cleaned_data.items():
-            if "_" in field_name:
-                field, lang_code = field_name.rsplit("_", 1)
-                translation, created = ExtensionTranslation.objects.get_or_create(
-                    extension=obj, language_code=lang_code
-                )
-                setattr(translation, field, value)
-                translation.save()
+        from DjangoProjectStsnDev import settings  # Подгружаем список языков
+        for lang_code, _ in settings.LANGUAGES:
+            translation, created = ExtensionTranslation.objects.get_or_create(
+                extension=obj, language_code=lang_code
+            )
+
+            # Обновляем переводы из формы
+            for field in ['name', 'description', 'short_description', 'title', 'meta_description']:
+                form_field = f"{field}_{lang_code}"
+                if form_field in form.cleaned_data:
+                    setattr(translation, field, form.cleaned_data[form_field])
+
+            translation.save()
 
 
 admin.site.register(Extension, ExtensionAdmin)
-
 
---
+++ extensions/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from .models import Extension, ExtensionTranslation
+
+class ExtensionAdminForm(forms.ModelForm):
+    """
+    Форма редактирования Extension с динамическими полями переводов
+    (на втором этапе рендеринга).
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance.pk:  # Только если это не новая запись
+            translations = ExtensionTranslation.objects.filter(extension=self.instance)
+            existing_langs = {t.language_code for t in translations}
+
+            from DjangoProjectStsnDev import settings  # Получаем список доступных языков
+            for lang_code, lang_name in settings.LANGUAGES:
+                # Если перевода нет — создаем пустую запись
+                if lang_code not in existing_langs:
+                    ExtensionTranslation.objects.create(extension=self.instance, language_code=lang_code)
+
+            # После создания/обновления записей заново получаем переводы
+            translations = ExtensionTranslation.objects.filter(extension=self.instance)
+
+            for translation in translations:
+                lang_code = translation.language_code
+
+                self.fields[f'name_{lang_code}'] = forms.CharField(
+                    initial=translation.name,
+                    label=_('Name') + f" ({lang_code})",
+                    max_length=255,
+                    required=False
+                )
+
+                self.fields[f'description_{lang_code}'] = forms.CharField(
+                    initial=translation.description,
+                    label=_('Description') + f" ({lang_code})",
+                    widget=forms.Textarea,
+                    required=False
+                )
+
+                self.fields[f'short_description_{lang_code}'] = forms.CharField(
+                    initial=translation.short_description,
+                    label=_('Short Description') + f" ({lang_code})",
+                    max_length=255,
+                    required=False
+                )
+
+                self.fields[f'title_{lang_code}'] = forms.CharField(
+                    initial=translation.title,
+                    label=_('Title') + f" ({lang_code})",
+                    max_length=255,
+                    required=False
+                )
+
+                self.fields[f'meta_description_{lang_code}'] = forms.CharField(
+                    initial=translation.meta_description,
+                    label=_('Meta Description') + f" ({lang_code})",
+                    widget=forms.Textarea,
+                    required=False
+                )
+
+    class Meta:
+        model = Extension
+        fields = '__all__'
