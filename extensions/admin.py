5c95817efe89e7bec23b1bd987db3627548157ff
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-02 23:54:55 +0200

    Proxy Model / admin var 3

--- extensions/admin.py
+++ extensions/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import Extension, ExtensionTranslation, ExtensionProxy
 
+
 class ExtensionTranslationInline(admin.TabularInline):
     model = ExtensionTranslation
     extra = 1
     fields = ('language_code', 'name', 'description', 'short_description', 'title', 'meta_description')
 
+
 class ExtensionAdmin(admin.ModelAdmin):
     list_display = ('name', 'version', 'secret_key', 'trial_period_days')
     inlines = [ExtensionTranslationInline]
     search_fields = ('name', 'version', 'secret_key')
+
 
 class ExtensionProxyAdmin(ExtensionAdmin):
     list_display = (
@@ -21,25 +24,54 @@ class ExtensionProxyAdmin(ExtensionAdmin):
     )
     search_fields = ('name', 'description', 'short_description', 'title', 'meta_description')
 
+    fieldsets = (
+        ('Name', {
+            'fields': ('name_en', 'name_ru'),
+            'legend': 'Name in all languages',
+        }),
+        ('Description', {
+            'fields': ('description_en', 'description_ru'),
+            'legend': 'Description in all languages',
+        }),
+        ('Short Description', {
+            'fields': ('short_description_en', 'short_description_ru'),
+            'legend': 'Short Description in all languages',
+        }),
+        ('Title', {
+            'fields': ('title_en', 'title_ru'),
+            'legend': 'Title in all languages',
+        }),
+        ('Meta Description', {
+            'fields': ('meta_description_en', 'meta_description_ru'),
+            'legend': 'Meta Description in all languages',
+        }),
+    )
+
     def name_all_languages(self, obj):
         return f"EN: {obj.name_en} | RU: {obj.name_ru}"
+
     name_all_languages.short_description = 'Name'
 
     def description_all_languages(self, obj):
         return f"EN: {obj.description_en} | RU: {obj.description_ru}"
+
     description_all_languages.short_description = 'Description'
 
     def short_description_all_languages(self, obj):
         return f"EN: {obj.short_description_en} | RU: {obj.short_description_ru}"
+
     short_description_all_languages.short_description = 'Short Description'
 
     def title_all_languages(self, obj):
         return f"EN: {obj.title_en} | RU: {obj.title_ru}"
+
     title_all_languages.short_description = 'Title'
 
     def meta_description_all_languages(self, obj):
         return f"EN: {obj.meta_description_en} | RU: {obj.meta_description_ru}"
+
     meta_description_all_languages.short_description = 'Meta Description'
+
 
 admin.site.register(Extension, ExtensionAdmin)
 admin.site.register(ExtensionProxy, ExtensionProxyAdmin)
