3f9a22190e0061a9315c74cac8a9a9ad6ac30fd2
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-03 15:25:30 +0200

    rollback form, admin

--- extensions/admin.py
+++ extensions/admin.py
@@ -1,41 +1,29 @@
 from django.contrib import admin
-from django import forms
 from unfold.admin import ModelAdmin
-from .models import ExtensionProxy, ExtensionTranslation
+from .models import ExtensionProxy
 from .forms import ExtensionProxyForm
 
 @admin.register(ExtensionProxy)
 class ExtensionProxyAdmin(ModelAdmin):
     form = ExtensionProxyForm
     list_display = ('name', 'description_current_language')
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        translatable_fields = ExtensionTranslation.get_translatable_fields()
-        language_codes = [code for code, _ in ExtensionTranslation.LANGUAGE_CHOICES]
-
-        for field in translatable_fields:
-            for lang_code in language_codes:
-                field_name = f"{field}_{lang_code}"
-                if field_name not in form.base_fields:
-                    form.base_fields[field_name] = forms.CharField(required=False)
-
-        return form
-
-    def get_fieldsets(self, request, obj=None):
-        fieldsets = [
-            ("Основная информация", {
-                'fields': ('name', 'version', 'secret_key', 'trial_period_days'),
-            }),
-        ]
-        translatable_fields = ExtensionTranslation.get_translatable_fields()
-        language_codes = [code for code, _ in ExtensionTranslation.LANGUAGE_CHOICES]
-
-        for field in translatable_fields:
-            field_fields = []
-            for lang_code in language_codes:
-                field_name = f"{field}_{lang_code}"
-                field_fields.append(field_name)
-            fieldsets.append((field.capitalize(), {'fields': tuple(field_fields)}))
-
-        return fieldsets
+    fieldsets = (
+        ("Основная информация", {
+            'fields': ('name', 'version', 'secret_key', 'trial_period_days'),
+        }),
+        ("Название", {
+            'fields': ('name_en', 'name_ru', 'name_uk'),
+        }),
+        ("Заголовок", {
+            'fields': ('title_en', 'title_ru', 'title_uk'),
+        }),
+        ("Краткое описание", {
+            'fields': ('short_description_en', 'short_description_ru', 'short_description_uk'),
+        }),
+        ("Полное описание", {
+            'fields': ('description_en', 'description_ru', 'description_uk'),
+        }),
+        ("Поисковое описание", {
+            'fields': ('meta_description_en', 'meta_description_ru', 'meta_description_uk'),
+        }),
+    )
