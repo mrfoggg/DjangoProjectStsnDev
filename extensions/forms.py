29e15c2d3457ca135485c9d9998d7c9555dfbe0e
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-03 15:48:06 +0200

    optimization ExtensionProxyForm __init__ and save + __init__ instead Meta

--- extensions/forms.py
+++ extensions/forms.py
@@ -4,9 +4,14 @@ from .models import ExtensionProxy, ExtensionTranslation
 from unfold.contrib.forms.widgets import WysiwygWidget
 from unfold.widgets import UnfoldAdminTextInputWidget
 
-class ExtensionProxyFormMeta(type(forms.ModelForm)):
-    def __new__(cls, name, bases, attrs):
-        new_class = super().__new__(cls, name, bases, attrs)
+class ExtensionProxyForm(forms.ModelForm):
+    class Meta:
+        model = ExtensionProxy
+        fields = ['name', 'version', 'secret_key', 'trial_period_days']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get('instance')
         translatable_fields = ExtensionTranslation.get_translatable_fields()
         language_codes = [code for code, _ in ExtensionTranslation.LANGUAGE_CHOICES]
 
@@ -16,29 +21,13 @@ class ExtensionProxyFormMeta(type(forms.ModelForm)):
                 label = lang_code.upper()
                 widget = WysiwygWidget if field in ['description', 'short_description', 'meta_description'] else UnfoldAdminTextInputWidget
 
-                new_class.base_fields[field_name] = forms.CharField(
+                self.fields[field_name] = forms.CharField(
                     label=label,
                     required=False,
                     widget=widget
                 )
 
-        return new_class
-
-class ExtensionProxyForm(forms.ModelForm, metaclass=ExtensionProxyFormMeta):
-    class Meta:
-        model = ExtensionProxy
-        fields = ['name', 'version', 'secret_key', 'trial_period_days']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
-        if instance:
-            translatable_fields = ExtensionTranslation.get_translatable_fields()
-            language_codes = [code for code, _ in ExtensionTranslation.LANGUAGE_CHOICES]
-
-            for field in translatable_fields:
-                for lang_code in language_codes:
-                    field_name = f"{field}_{lang_code}"
+                if instance:
                     translation = instance.get_translation(lang_code)
                     if translation:
                         self.fields[field_name].initial = getattr(translation, field)
@@ -59,4 +48,3 @@ class ExtensionProxyForm(forms.ModelForm, metaclass=ExtensionProxyFormMeta):
                     instance.set_translation(lang_code, field, value)
 
         return instance
-
