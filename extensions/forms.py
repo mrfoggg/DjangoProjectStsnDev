6eb06a3b9ec5c1b82a7bbc96a5b2e7ba77c75299
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-03 19:43:39 +0200

    optimization ExtensionProxyForm - ModelFormMetaclass2

--- extensions/forms.py
+++ extensions/forms.py
@@ -11,25 +11,24 @@ from unfold.widgets import UnfoldAdminTextInputWidget
 
 class ExtensionProxyFormMeta(forms.models.ModelFormMetaclass):
     def __new__(cls, name, bases, attrs):
-        # Создаем класс формы с использованием метакласса
+        # Создаем форму через метакласс
         new_class = super().__new__(cls, name, bases, attrs)
 
-        # Сначала собираем дополнительные поля для перевода
+        # Собираем новые поля
         additional_fields = {}
         translatable_fields = ExtensionTranslation.get_translatable_fields()
         language_codes = [code for code, _ in settings.LANGUAGES]
 
         for field in translatable_fields:
             for lang_code in language_codes:
-                # Динамически создаем поле для каждого языка
                 field_name = f"{field}_{lang_code}"
                 field_instance = forms.CharField(label=field_name, required=False)
                 additional_fields[field_name] = field_instance
 
-        # Добавляем новые поля в attrs формы
+        # Обновляем атрибуты класса, добавляя новые поля
         attrs.update(additional_fields)
 
-        # Обновляем Meta.fields с учетом новых полей
+        # Обновляем Meta.fields, чтобы включить новые поля
         if 'Meta' in attrs:
             if hasattr(attrs['Meta'], 'fields'):
                 attrs['Meta'].fields.extend(additional_fields.keys())
@@ -37,6 +36,7 @@ class ExtensionProxyFormMeta(forms.models.ModelFormMetaclass):
                 attrs['Meta'].fields = list(additional_fields.keys())
 
         return new_class
+
 
 class ExtensionProxyForm(forms.ModelForm, metaclass=ExtensionProxyFormMeta):
     def __init__(self, *args, **kwargs):
