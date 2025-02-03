f5b4823feb9dd94a2ce4149d247e2f39e820ee30
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-03 02:26:49 +0200

    model optimization  test dynamic props

--- extensions/forms.py
+++ extensions/forms.py
@@ -5,69 +5,24 @@ from unfold.contrib.forms.widgets import WysiwygWidget
 from unfold.widgets import UnfoldAdminTextInputWidget
 
 
-# class ExtensionProxyForm(forms.ModelForm):
-#     name_en = forms.CharField(
-#         label="EN",
-#         required=False,
-#         widget=UnfoldAdminTextInputWidget
-#     )
-#     description_en = forms.CharField(
-#         label="EN",
-#         required=False,
-#         widget=WysiwygWidget
-#     )
-#     name_ru = forms.CharField(
-#         label="RU",
-#         required=False,
-#         widget=UnfoldAdminTextInputWidget
-#     )
-#     description_ru = forms.CharField(
-#         label="RU",
-#         required=False,
-#         widget=WysiwygWidget
-#     )
-#
-#     class Meta:
-#         model = ExtensionProxy
-#         fields = ['name', 'version', 'secret_key', 'trial_period_days']
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         instance = kwargs.get('instance')
-#         if instance:
-#             self.fields['name_en'].initial = instance.get_translation('en').name if instance.get_translation('en') else ''
-#             self.fields['description_en'].initial = instance.get_translation('en').description if instance.get_translation('en') else ''
-#             self.fields['name_ru'].initial = instance.get_translation('ru').name if instance.get_translation('ru') else ''
-#             self.fields['description_ru'].initial = instance.get_translation('ru').description if instance.get_translation('ru') else ''
-#
-#     def save(self, commit=True):
-#         instance = super().save(commit=False)
-#         instance.set_translation('en', 'name', self.cleaned_data['name_en'])
-#         instance.set_translation('en', 'description', self.cleaned_data['description_en'])
-#         instance.set_translation('ru', 'name', self.cleaned_data['name_ru'])
-#         instance.set_translation('ru', 'description', self.cleaned_data['description_ru'])
-#         if commit:
-#             instance.save()
-#         return instance
-
 class ExtensionProxyForm(forms.ModelForm):
     name_en = forms.CharField(
-        label="Название (EN)",
+        label="EN",
         required=False,
         widget=UnfoldAdminTextInputWidget
     )
     description_en = forms.CharField(
-        label="Описание (EN)",
+        label="EN",
         required=False,
         widget=WysiwygWidget
     )
     name_ru = forms.CharField(
-        label="Название (RU)",
+        label="RU",
         required=False,
         widget=UnfoldAdminTextInputWidget
     )
     description_ru = forms.CharField(
-        label="Описание (RU)",
+        label="RU",
         required=False,
         widget=WysiwygWidget
     )
@@ -80,26 +35,71 @@ class ExtensionProxyForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         instance = kwargs.get('instance')
         if instance:
-            self.fields['name_en'].initial = instance.get_translation('en').name if instance.get_translation(
-                'en') else ''
-            self.fields['description_en'].initial = instance.get_translation(
-                'en').description if instance.get_translation('en') else ''
-            self.fields['name_ru'].initial = instance.get_translation('ru').name if instance.get_translation(
-                'ru') else ''
-            self.fields['description_ru'].initial = instance.get_translation(
-                'ru').description if instance.get_translation('ru') else ''
+            self.fields['name_en'].initial = instance.get_translation('en').name if instance.get_translation('en') else ''
+            self.fields['description_en'].initial = instance.get_translation('en').description if instance.get_translation('en') else ''
+            self.fields['name_ru'].initial = instance.get_translation('ru').name if instance.get_translation('ru') else ''
+            self.fields['description_ru'].initial = instance.get_translation('ru').description if instance.get_translation('ru') else ''
 
     def save(self, commit=True):
-        # Получаем объект без немедленного сохранения
         instance = super().save(commit=False)
-
-        # Обновляем переводы
         instance.set_translation('en', 'name', self.cleaned_data['name_en'])
         instance.set_translation('en', 'description', self.cleaned_data['description_en'])
         instance.set_translation('ru', 'name', self.cleaned_data['name_ru'])
         instance.set_translation('ru', 'description', self.cleaned_data['description_ru'])
-
-        # Сохраняем объект
-        instance.save()
-
+        if commit:
+            instance.save()
         return instance
+
+# class ExtensionProxyForm(forms.ModelForm):
+#     name_en = forms.CharField(
+#         label="Название (EN)",
+#         required=False,
+#         widget=UnfoldAdminTextInputWidget
+#     )
+#     description_en = forms.CharField(
+#         label="Описание (EN)",
+#         required=False,
+#         widget=WysiwygWidget
+#     )
+#     name_ru = forms.CharField(
+#         label="Название (RU)",
+#         required=False,
+#         widget=UnfoldAdminTextInputWidget
+#     )
+#     description_ru = forms.CharField(
+#         label="Описание (RU)",
+#         required=False,
+#         widget=WysiwygWidget
+#     )
+#
+#     class Meta:
+#         model = ExtensionProxy
+#         fields = ['name', 'version', 'secret_key', 'trial_period_days']
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         instance = kwargs.get('instance')
+#         if instance:
+#             self.fields['name_en'].initial = instance.get_translation('en').name if instance.get_translation(
+#                 'en') else ''
+#             self.fields['description_en'].initial = instance.get_translation(
+#                 'en').description if instance.get_translation('en') else ''
+#             self.fields['name_ru'].initial = instance.get_translation('ru').name if instance.get_translation(
+#                 'ru') else ''
+#             self.fields['description_ru'].initial = instance.get_translation(
+#                 'ru').description if instance.get_translation('ru') else ''
+#
+#     def save(self, commit=True):
+#         # Получаем объект без немедленного сохранения
+#         instance = super().save(commit=False)
+#
+#         # Обновляем переводы
+#         instance.set_translation('en', 'name', self.cleaned_data['name_en'])
+#         instance.set_translation('en', 'description', self.cleaned_data['description_en'])
+#         instance.set_translation('ru', 'name', self.cleaned_data['name_ru'])
+#         instance.set_translation('ru', 'description', self.cleaned_data['description_ru'])
+#
+#         # Сохраняем объект
+#         instance.save()
+#
+#         return instance
--- extensions/models.py
+++ extensions/models.py
@@ -49,60 +49,41 @@ class ExtensionTranslation(models.Model):
         return ['name', 'title', 'short_description', 'description', 'meta_description']
 
 
-#
-# class ExtensionProxy(Extension):
-#     class Meta:
-#         proxy = True  # Указываем, что модель будет прокси и не создаст новую таблицу
-#
-#     # Виртуальные поля для каждого языка
-#     @property
-#     def name_en(self):
-#         return self.get_translation('en').name if self.get_translation('en') else None
-#
-#     @property
-#     def description_en(self):
-#         return self.get_translation('en').description if self.get_translation('en') else None
-#
-#     @property
-#     def name_ru(self):
-#         return self.get_translation('ru').name if self.get_translation('ru') else None
-#
-#     @property
-#     def description_ru(self):
-#         return self.get_translation('ru').description if self.get_translation('ru') else None
-#
-#     def get_translation(self, language_code):
-#         """Возвращает перевод для заданного языка, если он существует."""
-#         return self.translations.filter(language_code=language_code).first()
-#
-#     def set_translation(self, language_code, field, value):
-#         """Добавляем метод для сохранения переводов"""
-#         translation = self.get_translation(language_code)
-#         if translation:
-#             setattr(translation, field, value)
-#             translation.save()
-#         else:
-#             # Если перевода нет, создаем новый
-#             translation = self.translations.create(language_code=language_code)
-#             setattr(translation, field, value)
-#             translation.save()
 
 class ExtensionProxy(Extension):
     class Meta:
         proxy = True  # Указываем, что модель будет прокси и не создаст новую таблицу
 
-    # Виртуальные поля для каждого языка
+    languages = ['en', 'ru', 'fr']  # Список языков
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Создание динамических свойств для каждого языка
+        for lang in self.languages:
+            # Создаем и устанавливаем атрибуты для каждого языка
+            setattr(self, f"description_{lang}",
+                    self.get_translation(lang).description if self.get_translation(lang) else None)
+
+
+    # # Виртуальные поля для каждого языка
+    # @property
+    # def name_en(self):
+    #     return self.get_translation('en').name if self.get_translation('en') else None
+    #
+    # @property
+    # def description_en(self):
+    #     return self.get_translation('en').description if self.get_translation('en') else None
+
+
+
     @property
-    def translations_data(self):
-        translations = {}
-        for field in self._meta.get_fields():  # Получаем все поля модели
-            # Исключаем поля language_code и extension (основные поля для перевода)
-            if field.name not in ['language_code', 'extension', 'id']:
-                translations[field.name] = {
-                    'en': getattr(self.get_translation('en'), field.name, None),
-                    'ru': getattr(self.get_translation('ru'), field.name, None),
-                }
-        return translations
+    def name_ru(self):
+        return self.get_translation('ru').name if self.get_translation('ru') else None
+
+    @property
+    def description_ru(self):
+        return self.get_translation('ru').description if self.get_translation('ru') else None
 
     def get_translation(self, language_code):
         """Возвращает перевод для заданного языка, если он существует."""
@@ -119,13 +100,46 @@ class ExtensionProxy(Extension):
             translation = self.translations.create(language_code=language_code)
             setattr(translation, field, value)
             translation.save()
-
-    def save(self, commit=True):
-        # Проходим по всем полям модели и сохраняем переводы
-        for field in self._meta.get_fields():  # Все поля модели
-            if field.name not in ['language_code', 'extension', 'id']:  # Исключаем ненужные поля
-                self.set_translation('en', field.name, getattr(self, f'{field.name}_en', ''))
-                self.set_translation('ru', field.name, getattr(self, f'{field.name}_ru', ''))
-
-        return super().save(commit=commit)
+#
+# class ExtensionProxy(Extension):
+#     class Meta:
+#         proxy = True  # Указываем, что модель будет прокси и не создаст новую таблицу
+#
+#     # Виртуальные поля для каждого языка
+#     @property
+#     def translations_data(self):
+#         translations = {}
+#         for field in self._meta.get_fields():  # Получаем все поля модели
+#             # Исключаем поля language_code и extension (основные поля для перевода)
+#             if field.name not in ['language_code', 'extension', 'id']:
+#                 translations[field.name] = {
+#                     'en': getattr(self.get_translation('en'), field.name, None),
+#                     'ru': getattr(self.get_translation('ru'), field.name, None),
+#                 }
+#         return translations
+#
+#     def get_translation(self, language_code):
+#         """Возвращает перевод для заданного языка, если он существует."""
+#         return self.translations.filter(language_code=language_code).first()
+#
+#     def set_translation(self, language_code, field, value):
+#         """Добавляем метод для сохранения переводов"""
+#         translation = self.get_translation(language_code)
+#         if translation:
+#             setattr(translation, field, value)
+#             translation.save()
+#         else:
+#             # Если перевода нет, создаем новый
+#             translation = self.translations.create(language_code=language_code)
+#             setattr(translation, field, value)
+#             translation.save()
+#
+#     def save(self):
+#         # Проходим по всем полям модели и сохраняем переводы
+#         for field in self._meta.get_fields():  # Все поля модели
+#             if field.name not in ['language_code', 'extension', 'id']:  # Исключаем ненужные поля
+#                 self.set_translation('en', field.name, getattr(self, f'{field.name}_en', ''))
+#                 self.set_translation('ru', field.name, getattr(self, f'{field.name}_ru', ''))
+#
+#         return super().save()
 
