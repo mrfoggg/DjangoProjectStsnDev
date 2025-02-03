fdc32ac8ba06b484b0658cf322e044a106a9e95e
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-03 18:51:33 +0200

    optimization ExtensionProxyForm __init__ and save + selfRealization Meta _back-test

--- extensions/forms.py
+++ extensions/forms.py
@@ -6,21 +6,99 @@ from .models import ExtensionProxy, ExtensionTranslation
 from unfold.contrib.forms.widgets import WysiwygWidget
 from unfold.widgets import UnfoldAdminTextInputWidget
 
-class ExtensionProxyFormMeta(type(forms.ModelForm)):
-    def __new__(cls, name, bases, attrs):
-        new_class = super().__new__(cls, name, bases, attrs)
-        for model_filed in ExtensionTranslation._meta.fields:
-            if isinstance(model_filed, (CharField, TextField)) and not hasattr(model_filed, 'choices'):
-                form_field = model_filed.formfield()
-                form_field_kwargs = form_field.__dict__.copy()
-                for lang_code in [code for code, _ in ExtensionTranslation.LANGUAGE_CHOICES]:
-                    new_form_field = form_field.__class__(**form_field_kwargs)
-                    field_name = f"{model_filed.name}_{lang_code}"
-                    setattr(ExtensionProxyFormMeta, field_name, new_form_field)
-        return new_class
+# class ExtensionProxyFormMeta(type(forms.ModelForm)):
+#     def __new__(cls, name, bases, attrs):
+#         new_class = super().__new__(cls, name, bases, attrs)
+#         for model_filed in ExtensionTranslation._meta.fields:
+#             if isinstance(model_filed, (CharField, TextField)) and not hasattr(model_filed, 'choices'):
+#                 form_field = model_filed.formfield()
+#                 form_field_kwargs = form_field.__dict__.copy()
+#                 for lang_code in [code for code, _ in ExtensionTranslation.LANGUAGE_CHOICES]:
+#                     new_form_field = form_field.__class__(**form_field_kwargs)
+#                     field_name = f"{model_filed.name}_{lang_code}"
+#                     setattr(ExtensionProxyFormMeta, field_name, new_form_field)
+#         return new_class
 
 
-class ExtensionProxyForm(forms.ModelForm, metaclass=ExtensionProxyFormMeta):
+# class ExtensionProxyForm(forms.ModelForm, metaclass=ExtensionProxyFormMeta):
+class ExtensionProxyForm(forms.ModelForm):
+    name_en = forms.CharField(
+        label="EN",
+        required=False,
+        widget=UnfoldAdminTextInputWidget
+    )
+    description_en = forms.CharField(
+        label="EN",
+        required=False,
+        widget=WysiwygWidget
+    )
+    short_description_en = forms.CharField(
+        label="EN",
+        required=False,
+        widget=WysiwygWidget
+    )
+    title_en = forms.CharField(
+        label="EN",
+        required=False,
+        widget=UnfoldAdminTextInputWidget
+    )
+    meta_description_en = forms.CharField(
+        label="EN",
+        required=False,
+        widget=WysiwygWidget
+    )
+
+    name_ru = forms.CharField(
+        label="RU",
+        required=False,
+        widget=UnfoldAdminTextInputWidget
+    )
+    description_ru = forms.CharField(
+        label="RU",
+        required=False,
+        widget=WysiwygWidget
+    )
+    short_description_ru = forms.CharField(
+        label="RU",
+        required=False,
+        widget=WysiwygWidget
+    )
+    title_ru = forms.CharField(
+        label="RU",
+        required=False,
+        widget=UnfoldAdminTextInputWidget
+    )
+    meta_description_ru = forms.CharField(
+        label="RU",
+        required=False,
+        widget=WysiwygWidget
+    )
+
+    name_uk = forms.CharField(
+        label="UA",
+        required=False,
+        widget=UnfoldAdminTextInputWidget
+    )
+    description_uk = forms.CharField(
+        label="UA",
+        required=False,
+        widget=WysiwygWidget
+    )
+    short_description_uk = forms.CharField(
+        label="UA",
+        required=False,
+        widget=WysiwygWidget
+    )
+    title_uk = forms.CharField(
+        label="UA",
+        required=False,
+        widget=UnfoldAdminTextInputWidget
+    )
+    meta_description_uk = forms.CharField(
+        label="UA",
+        required=False,
+        widget=WysiwygWidget
+    )
     class Meta:
         model = ExtensionProxy
         fields = ['name', 'version', 'secret_key', 'trial_period_days']
