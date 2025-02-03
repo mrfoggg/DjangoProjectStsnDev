5a83d7d87d328a4f9106b21139b460b98ec158ac
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-03 15:20:15 +0200

    ExtensionProxyFormMeta + admin 4

--- extensions/admin.py
+++ extensions/admin.py
@@ -23,43 +23,19 @@ class ExtensionProxyAdmin(ModelAdmin):
         return form
 
     def get_fieldsets(self, request, obj=None):
-        fieldsets = super().get_fieldsets(request, obj)
+        fieldsets = [
+            ("Основная информация", {
+                'fields': ('name', 'version', 'secret_key', 'trial_period_days'),
+            }),
+        ]
         translatable_fields = ExtensionTranslation.get_translatable_fields()
         language_codes = [code for code, _ in ExtensionTranslation.LANGUAGE_CHOICES]
 
         for field in translatable_fields:
+            field_fields = []
             for lang_code in language_codes:
                 field_name = f"{field}_{lang_code}"
-                if field == 'name':
-                    fieldsets[1][1]['fields'] += (field_name,)
-                elif field == 'title':
-                    fieldsets[2][1]['fields'] += (field_name,)
-                elif field == 'short_description':
-                    fieldsets[3][1]['fields'] += (field_name,)
-                elif field == 'description':
-                    fieldsets[4][1]['fields'] += (field_name,)
-                elif field == 'meta_description':
-                    fieldsets[5][1]['fields'] += (field_name,)
+                field_fields.append(field_name)
+            fieldsets.append((field.capitalize(), {'fields': tuple(field_fields)}))
 
         return fieldsets
-
-    fieldsets = (
-        ("Основная информация", {
-            'fields': ('name', 'version', 'secret_key', 'trial_period_days'),
-        }),
-        ("Название", {
-            'fields': tuple(),
-        }),
-        ("Заголовок", {
-            'fields': tuple(),
-        }),
-        ("Краткое описание", {
-            'fields': tuple(),
-        }),
-        ("Полное описание", {
-            'fields': tuple(),
-        }),
-        ("Поисковое описание", {
-            'fields': tuple(),
-        }),
-    )
