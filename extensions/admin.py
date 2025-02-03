32fb29da20b4d5a0a86b6ed9a0961c9cbe50b8ab
thinkpad-fedora <mrfoggg@gmail.com>
2025-02-03 12:06:42 +0200

    CL variant 1 fix 4

--- extensions/admin.py
+++ extensions/admin.py
@@ -15,51 +15,47 @@ class ExtensionAdminForm(forms.ModelForm):
         fields = ['name', 'version', 'secret_key', 'trial_period_days', 'file']
 
 
-@admin.register(Extension)
-class ExtensionAdmin(ModelAdmin):
-    form = ExtensionAdminForm
-    list_display = ('name', 'version', 'trial_period_days')
+class ExtensionAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = kwargs.get('instance')
 
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-
-        # Словарь для хранения полей перевода
-        translation_fields = {}
-
+        # Add translation fields dynamically
         for lang_code, _ in settings.LANGUAGES:
-            for field_name in ['name', 'title', 'short_description', 'description', 'meta_description']:
-                # Создаем имя поля перевода
-                trans_field_name = f'{field_name}_{lang_code}'
+            for field_name in ExtensionTranslation.get_translatable_fields():
+                field_key = f'{field_name}_{lang_code}'
 
-                # Определяем виджет в зависимости от типа поля
-                if field_name in ['description', 'meta_description']:
-                    widget = forms.Textarea
-                else:
-                    widget = forms.TextInput
+                # Set appropriate widget based on field type
+                widget = forms.Textarea if field_name in ['description', 'meta_description'] else forms.TextInput
 
-                # Создаем поле формы
-                translation_fields[trans_field_name] = forms.CharField(
+                self.fields[field_key] = forms.CharField(
                     required=False,
                     widget=widget(),
                     label=f"{field_name.replace('_', ' ').title()} ({lang_code.upper()})"
                 )
 
-                # Добавляем начальные данные, если объект существует
-                if obj:
+                # Set initial values if instance exists
+                if instance:
                     try:
-                        trans = obj.translations.get(language_code=lang_code)
-                        translation_fields[trans_field_name].initial = getattr(trans, field_name, '')
+                        translation = instance.translations.get(language_code=lang_code)
+                        self.fields[field_key].initial = getattr(translation, field_name, '')
                     except ExtensionTranslation.DoesNotExist:
                         pass
 
-        # Добавляем поля в форму
-        form.base_fields.update(translation_fields)
+    class Meta:
+        model = Extension
+        fields = ['name', 'version', 'secret_key', 'trial_period_days', 'file']
 
-        return form
+
+@admin.register(Extension)
+class ExtensionAdmin(ModelAdmin):
+    form = ExtensionAdminForm
+    list_display = ('name', 'version', 'trial_period_days')
 
     def get_fieldsets(self, request, obj=None):
+        # Base fieldset with model fields
         fieldsets = [
-            (None, {
+            (_('Basic Information'), {
                 'fields': [
                     'name', 'version', 'secret_key',
                     'trial_period_days', 'file'
@@ -67,21 +63,23 @@ class ExtensionAdmin(ModelAdmin):
             })
         ]
 
-        # Группируем поля по типу (название, описание и т.д.)
-        field_groups = [
-            ('Название', 'name'),
-            ('Заголовок', 'title'),
-            ('Краткое описание', 'short_description'),
-            ('Описание', 'description'),
-            ('Мета-описание', 'meta_description')
-        ]
+        # Translation fieldsets
+        translation_groups = {
+            _('Names'): ['name'],
+            _('Titles'): ['title'],
+            _('Short Descriptions'): ['short_description'],
+            _('Descriptions'): ['description'],
+            _('Meta Descriptions'): ['meta_description']
+        }
 
-        # Создаем группы полей для каждого типа перевода
-        for group_label, field_name in field_groups:
-            translation_fields = [
-                f'{field_name}_{lang_code}'
-                for lang_code, _ in settings.LANGUAGES
-            ]
+        for group_label, field_names in translation_groups.items():
+            translation_fields = []
+            for field_name in field_names:
+                translation_fields.extend([
+                    f'{field_name}_{lang_code}'
+                    for lang_code, _ in settings.LANGUAGES
+                ])
+
             fieldsets.append((
                 group_label,
                 {
@@ -95,19 +93,17 @@ class ExtensionAdmin(ModelAdmin):
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
 
-        # Сохраняем переводы
+        # Save translations
         for lang_code, _ in settings.LANGUAGES:
             translation_data = {}
-            has_translation_data = False
 
-            for field_name in ['name', 'title', 'short_description', 'description', 'meta_description']:
+            for field_name in ExtensionTranslation.get_translatable_fields():
                 trans_field_name = f'{field_name}_{lang_code}'
                 value = form.cleaned_data.get(trans_field_name)
-                if value:
+                if value is not None:  # Allow empty strings but not None
                     translation_data[field_name] = value
-                    has_translation_data = True
 
-            if has_translation_data:
+            if translation_data:
                 ExtensionTranslation.objects.update_or_create(
                     extension=obj,
                     language_code=lang_code,
